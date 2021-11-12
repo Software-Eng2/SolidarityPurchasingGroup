f89e2ae240795b32444b0349dee88dd801b3d93e
cristianernestoo <s281697@studenti.polito.it>
2021-11-12 18:02:48 +0100

    fixed homepage
    todo: fix navbar

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,19 +1,25 @@
-import { Carousel, Card, CardGroup, Container, Row, Col, Button } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
+import { Card, CardGroup, Container, Row, Col, Button } from 'react-bootstrap';
+import { useState} from 'react';
 import backgroundimg from '../img/finale.mp4';
-function Homepage() {
-    const [index, setIndex] = useState(0);
+import { arrowIcon } from './Icons';
+import { Link } from 'react-router-dom';
 
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex);
-    };
+function Homepage() {
+    
     return (
         <div>
-            <div style={{position:"relative",overflowX:"hidden"}}>
-                <video style={{maxHeight:"90vh",width:"100vw"}} autoPlay loop muted>
+            <div style={{ overflowX: "hidden", backgroundColor: "#DEDEDE", position: "relative" }}>
+                <video style={{ maxHeight: "100vh", width: "100vw" }} autoPlay loop muted>
                     <source src={backgroundimg} type='video/mp4' />
                 </video>
-
+                <div className="overlay">
+                    <h1 style={{ color: "#ffffff" }}>
+                        Welcome to SPG
+                    </h1>
+                </div>
+                <div className="overlayArrow">
+                    {arrowIcon}
+                </div>
             </div>
             <CardGroup style={{ marginTop: "12vh", marginBottom: "12vh" }}>
                 <Container fluid="md">
@@ -24,14 +30,16 @@ function Homepage() {
                             </Card>
                         </Col>
                         <Col style={{ textAlign: "center" }}>
-                            <h1 style={{ color: "#ba0000" }}>
+                            <h1 style={{ color: "#247D37" }}>
                                 Taste the equality
                             </h1>
                             <h4 style={{ marginTop: "7vh" }}>
                                 It's local. It's fair. It's for our planet.
                             </h4>
-                            <Button size="lg" variant="danger" style={{ backgroundColor: "#BA0000", marginTop: "7vh" }} >
-                                Discover our products!
+                            <Button size="lg" variant="success" style={{ backgroundColor: "#247D37", marginTop: "7vh" }} >
+                                <Link to={{ pathname: '/products' }} className="font-weight-light" style={{ color: "white", textDecoration: "none" }}>
+                                    Discover our products
+                                </Link>
                             </Button>
                         </Col>
                     </Row>
@@ -39,54 +47,63 @@ function Homepage() {
             </CardGroup>
             <div style={{ backgroundColor: "#DEDEDE", width: "100%" }}>
                 <Container>
-                    <Row style={{ textAlign: "center" }} >
-                        <h1 style={{ color: "#ba0000" }}>
+                    <Row style={{ textAlign: "center", justifyContent: "center" }} >
+                        <h1 style={{ color: "#247D37", marginTop: "2rem" }}>
                             JOIN US!
                         </h1>
                     </Row>
-                    <Row style={{ marginTop: "10vh" }}>
+                    <Row style={{ marginTop: "2rem" }}>
                         <Col>
-                            <Card>
-                                <Card.Img variant="top" src="https://www.altromercato.it/wp-content/uploads/2021/09/produttore_Hualtaco_Peru_AgrofairEurope-scaled-e1632143818906-800x582.jpg" />
+                            <Card style={{maxHeight:"65vh", minHeight:"65vh"}}>
+                                <Card.Img variant="top" className="cardHome" src="https://images.pexels.com/photos/3962283/pexels-photo-3962283.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
                                 <Card.Body>
-                                    <Card.Title>Why selling in SPG?</Card.Title>
+                                    <Card.Title style={{ overflowX: "hidden", overflowY: "hidden", textAlign: "center" }}>Why buying in SPG?</Card.Title>
                                     <Card.Text>
-                                        You decide the price and all orders are placed before distribution. So you will bring only what has been planned, and you will leave without any unsold goods.
+                                    In SPG the products travel an average of 61 kilometers before reaching the plate. Each territory holds a treasure of local products.
                                     </Card.Text>
                                 </Card.Body>
                                 <Card.Footer>
-
-                                </Card.Footer>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card>
-                                <Card.Img variant="top" src="https://st2.depositphotos.com/1518767/8182/i/600/depositphotos_81826332-stock-photo-smiling-farmer-presenting-the-local.jpg" />
-                                <Card.Body>
-                                    <Card.Title>Taste the equality</Card.Title>
-                                    <Card.Text>
-                                        It's local. It's fair. It's for our planet.
-                                    </Card.Text>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <Button variant="primary" size="lg">
-                                        Discover our products!
+                                    <Button size="lg" variant="success" style={{ backgroundColor: "#247D37" }} >
+                                        <Link to={{ pathname: '/registerform' }} className="font-weight-light" style={{ color: "white", textDecoration: "none" }}>
+                                            Become our Client
+                                        </Link>
                                     </Button>
                                 </Card.Footer>
                             </Card>
                         </Col>
                         <Col>
-                            <Card>
-                                <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card style={{maxHeight:"65vh", minHeight:"65vh"}}>
+                                <Card.Img variant="top" className="cardHome" src="https://www.altromercato.it/wp-content/uploads/2021/09/produttore_Hualtaco_Peru_AgrofairEurope-scaled-e1632143818906-800x582.jpg" />
                                 <Card.Body>
-                                    <Card.Title>Card title</Card.Title>
+                                    <Card.Title style={{ overflowX: "hidden", overflowY: "hidden", textAlign: "center" }}>Why selling in SPG?</Card.Title>
                                     <Card.Text>
-                                        This is a wider card with supporting text below as a natural lead-in to
-                                        additional content. This content is a little bit longer.
+                                        You decide the price and all orders are placed before distribution. So you will bring only what has been planned, and you will leave without any unsold goods.
                                     </Card.Text>
                                 </Card.Body>
                                 <Card.Footer>
-                                    <small className="text-muted">Last updated 3 mins ago</small>
+                                    <Button size="lg" variant="success" style={{ backgroundColor: "#247D37" }} >
+                                        <Link to={{ pathname: '/' }} className="font-weight-light" style={{ color: "white", textDecoration: "none" }}>
+                                            Become our Supplier
+                                        </Link>
+                                    </Button>
+                                </Card.Footer>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card style={{maxHeight:"65vh", minHeight:"65vh"}}>
+                                <Card.Img variant="top" className="cardHome" src="https://images.pexels.com/photos/7844008/pexels-photo-7844008.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                                <Card.Body>
+                                    <Card.Title style={{ overflowX: "hidden", overflowY: "hidden", textAlign: "center" }}>Work with us</Card.Title>
+                                    <Card.Text>
+                                       Be part of the change.
+                                    </Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <Button size="lg" variant="success" style={{ backgroundColor: "#247D37" }} >
+                                        <Link to={{ pathname: '/' }} className="font-weight-light" style={{ color: "white", textDecoration: "none" }}>
+                                            Become our Delivery person
+                                        </Link>
+                                    </Button>
                                 </Card.Footer>
                             </Card>
                         </Col>
