e6e23b7f54dc1ef76319c0f19d0d7a74045a8f93
Francesco Policastro s281705 <s281705@studenti.polito.it>
2021-12-07 11:54:48 +0100

    updated Client Orders interface (still some detail to fix + API to be added)

--- client/src/components/ClientOrders/ClientOrders.js
+++ client/src/components/ClientOrders/ClientOrders.js
@@ -1,11 +1,14 @@
-import { useState } from 'react';
-import {Card, Button, Row, Col} from 'react-bootstrap'
-import {pickUpIcon, deliveryIcon, arrowRightIcon} from "../Icons";
+import { useState, useEffect } from 'react';
+import {Card, ButtonGroup, Button, Row, Col, ListGroup, ListGroupItem, Form} from 'react-bootstrap'
+import {pickUpIcon, deliveryIcon, arrowRightIcon, arrowLeftIcon, iconDelete, iconEdit, iconCross, iconConfirm} from "../Icons";
+import API from '../../API';
+import dayjs from 'dayjs';
 import gif from '../../img/preparation.gif'
 
+//area below table where are shown list of orders or the detail of one of them
 function ClientOrders(props) {
 
-    const clientOrders = props.clientOrders;
+    const {clientOrders} = props;
     const [selected, setSelected] = useState(false);
     const [order, setOrder] = useState();
 
@@ -37,7 +40,7 @@ function ClientOrders(props) {
         }
 }
 
-
+//list of pending orders
 function PendingList(props){
 
     const {clientOrders, handleClick, setOrder} = props;
@@ -108,24 +111,324 @@ function AcceptedList(props){
     );
 }
 
+//detail of selected order
 function SelectedOrder(props){
 
     const {handleClick, order} = props;
 
+    const [basket, setBasket] = useState([]);
+
+    const [delivery, setDelivery] = useState(order.pick_up);
+    const [address, setAddress] = useState(`${order.address.split(',')[0]}, ${order.address.split(',')[1]}`);
+    const [city, setCity] = useState(order.address.split(',')[2]);
+    const [zip, setZip] = useState(parseInt(order.address.split(',')[3]));
+    const [date, setDate] = useState(order.date);
+    const [time, setTime] = useState(order.time);
+    const [isDisabled, setIsDisabled] = useState(delivery);
+
+    const [edit, setEdit] = useState(false);
+    const [dirty, setDirty] = useState(true);
+
+    const sum = (key1, key2) => {
+        return basket.reduce((a, b) => a + (b[key1]*b[key2] || 0), 0);
+    }
+
+    let total = sum("price", "quantity");
+
+    useEffect(() => {
+        if(order){
+          API.getBasket(order.id).then((products) => {
+            console.log(products);
+            setBasket(products);
+          })
+        }
+    }, [order]);
+
+    useEffect(() => {
+        if (dirty) {
+            total = sum("price", "quantity");
+            setDirty(false);
+        }
+    }, [dirty]);
+
     return(
         <div className="my-3 mx-3">
-            <h1>Riepilogo Ordine:</h1>
-            <Row className="m-auto d-flex align-items-center"><h4>Delivery method:&ensp; </h4> {order.pick_up ? pickUpIcon /* Pick-Up */ : deliveryIcon /* Delivery */}</Row>
-            <Row className="m-auto d-flex align-items-center"><h4>Address:&ensp; </h4>  {order.address}</Row>
-            <Row className="m-auto d-flex align-items-center"><h4>Date:&ensp; </h4> {order.date}</Row>
-            <Row className="m-auto d-flex align-items-center"><h4>Time:&ensp; </h4> {order.time}</Row>
-            <h1>AGGIUNGERE POSSIBILITà DI MODIFICARE I CAMPI</h1>
-            <h1>AGGIUNGERE PRODOTTI + OZIONI PER MODIFICARLI</h1>
-            <h1>AGGIUNGERE BOTTONE PER ELIMINARE L'ORDINE</h1>
-            <h1>AGGIUNGERE BOTTONE PER SALVARE MODIFICHE</h1>
+            <Row>  
+                <Col><h1>Ordine #{order.id}</h1></Col>          
+                <Col xs={5} className="text-right"><h1>€ {total.toFixed(2)}</h1></Col>                    
+            </Row>
+            <hr/>
+            <Row>
+                <Col >
+                    {!edit ?
+                        <div xs={12} md={4} lg={4}>
+                            <Row className="m-auto d-flex align-items-center"><h4>Delivery method:&ensp; </h4> {order.pick_up ? pickUpIcon /* Pick-Up */ : deliveryIcon /* Delivery */}</Row>
+                            <Row className="m-auto d-flex align-items-center"><h4>Address:&ensp; </h4>  {order.address}</Row>
+                            <Row className="m-auto d-flex align-items-center"><h4>Date:&ensp; </h4> {order.date}</Row>
+                            <Row className="m-auto d-flex align-items-center"><h4>Time:&ensp; </h4> {order.time}</Row>
+                        </div>
+                        :
+                        <OrderForm
+                            delivery={delivery}
+                            setDelivery={setDelivery}
+                            address={address}
+                            setAddress={setAddress}
+                            city={city}
+                            setCity={setCity}
+                            zip={zip}
+                            setZip={setZip}
+                            date={date}
+                            setDate={setDate}
+                            time={time}
+                            setTime={setTime}
+                            isDisabled={isDisabled}
+                            setIsDisabled={setIsDisabled}
+                        />
+                    }
+                    
+                </Col>
+
+                <Col xs={12} lg={8} style={{ borderLeft: "2px solid #b4e6e2" }} >
+                    <ListGroup as="ul" className="mb-3">
+                        {
+                            basket.map((p) => (
+                                <ListIteam basket={basket} setBasket={setBasket} p={p} setDirty={setDirty} edit={edit} setEdit={setEdit} />
+                            ))
+                        }
+                    </ListGroup>
+                </Col>
+            </Row>
+            {!edit?
+            <Row className="mt-3">
+                <Col className="text-center">
+                    <Button variant="light" onClick={handleClick}>{arrowLeftIcon}Back</Button>
+                </Col>
+                <Col className="text-center">
+                    <Button variant="outline-dark" onClick={() => {  } }>{iconDelete} Cancel</Button> {/*TODO: onclick-> API cancella ordine */}
+                </Col>
+                <Col className="text-center">
+                        <Button variant="light"  onClick={() => { setEdit(!edit) }}>{iconEdit} Edit</Button> 
+                </Col>
+            </Row> :
+            <Row className="mt-3">
+                <Col className="text-center">
+                    <Button variant="outline-dark"  onClick={() => { setEdit(!edit) }}>{arrowLeftIcon}Cancel</Button>
+                </Col>
+                <Col className="text-center">
+                    <Button variant="outline-success"
+                      disabled={((delivery==='')||(address==='')||(city==='')||(zip==='')||(date==='')||(time==='')||(time<"09:00")||(time>"21:00"))? true : false}
+                      onClick={() => {  }} /* TODO: onclick API modifica ordine */
+                    >
+                        {iconConfirm} Confirm
+                    </Button> 
+                </Col>
+            </Row>
             
-            <Button onClick={handleClick}>Bottone per tornare alla lista</Button>
+
+            }
+            
         </div>
+    );
+}
+
+function OrderForm(props){
+
+    const {delivery, setDelivery, address, setAddress, city, setCity, zip, setZip, date, setDate, time, setTime, isDisabled, setIsDisabled } = props;
+
+    return (
+        <div xs={12} md={4} lg={4}>
+            <h6 className="text-center"><strong>Select a delivery method:</strong></h6>
+            <Form>
+                <Row className='justify-content-center mb-3'>
+                    <Col xs={6} md={6}>
+                        <div key="default-radio" className="text-left">
+                            <Form.Check
+                                inline
+                                label="Pick-Up"
+                                name="group1"
+                                type="radio"
+                                id="inline-radio-1"
+                                checked={delivery === 1 ? true : false}
+                                onClick={() => { setDelivery(1); setAddress("Corso Duca degli Abruzzi, 24"); setCity("Torino"); setZip("10129"); setIsDisabled(true); }}
+                            />
+                        </div>
+                    </Col>
+                    <Col xs={6} md={6}>
+                        <div key="default-radio" className="text-right">
+                            <Form.Check
+                                inline
+                                label="Delivery"
+                                name="group1"
+                                type="radio"
+                                id="inline-radio-2"
+                                checked={delivery === 1 ? false : true}
+                                onClick={() => { setDelivery(0); setAddress(''); setCity(''); setZip(''); setIsDisabled(false); }}
+                            />
+                        </div>
+                    </Col>
+                </Row>
+                <h6 className="text-center"><strong>Insert an Address:</strong></h6>
+                <Row>
+                    <Col xs={12} md={12}>
+                        <Form.Group className="mb-1" controlId="formGridAddress1">
+                            <Form.Control
+                                placeholder="1234 Main St"
+                                value={address}
+                                /* value={delivery===1 ? "Corso Duca degli Abruzzi, 24" : ""}      */
+                                onChange={(event) => setAddress(event.target.value)}
+                                disabled={isDisabled}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={6} md={6}>
+                        <Form.Group className="mb-1" controlId="formGridAddress1">
+                            <Form.Control
+                                placeholder="City"
+                                value={city}
+                                /* value={delivery===1 ? "Torino" : ""} */
+                                onChange={(event) => setCity(event.target.value)}
+                                disabled={isDisabled}
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col xs={6} md={6}>
+                        <Form.Group className="mb-1" controlId="formGridAddress2">
+                            <Form.Control
+                                placeholder="ZIP"
+                                value={zip}
+                                type="number"
+                                onChange={(event) => setZip(event.target.value)}
+                                disabled={isDisabled}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <h6 className="text-center"><strong>Select a pick-up/dalivery date & time:</strong></h6>
+                <Row className='justify-content-center mt-3'>
+                    <Col xs={8} md={8}>
+                        <Form.Group className="mb-1" controlId="formDate">
+                            <Form.Control
+                                type="date"
+                                placeholder="date"
+                                value={date}
+                                data-testid="calendar"
+                                onChange={(event) => { setDate(event.target.value); }}
+                                min={dayjs().add(1, "w").day(3).format("YYYY-MM-DD")}
+                                max={dayjs().add(1, "w").day(5).format("YYYY-MM-DD")} />
+                        </Form.Group>
+                    </Col>
+                    <Col xs={8} md={8}>
+                        <Form.Group className="mb-1" controlId="formDate">
+                            <Form.Control
+                                type="time"
+                                placeholder="time"
+                                data-testid="time"
+                                value={time}
+                                min="09:00"
+                                max="21:00"
+                                onChange={(event) => { setTime(event.target.value); }} />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row className="text-center mb-2"><Col xs={12} md={12} lg={12}><small>Please, pick a time between 09:00 and 21:00</small></Col></Row>
+            </Form>
+        </div>
+    );
+}
+
+
+function ListIteam(props) {
+
+    const {p, basket, setBasket, setDirty, edit, setEdit} = props;
+    const [quantity, setQuantity] = useState(p.quantity);
+
+    const decreaseValue = (p) => {
+        if (quantity > 1) {
+            let value = quantity;
+            setQuantity(--value);
+            let isProduct = (product) => product == p;
+            let index = basket.findIndex(isProduct);
+            basket[index].quantity = value;
+            setBasket(basket);
+            setDirty(true);
+        }
+    }
+
+    const increaseValue = (p) => {
+        if (quantity < p.availability) {
+            let value = quantity;
+            setQuantity(++value);
+            let isProduct = (product) => product == p;
+            let index = basket.findIndex(isProduct);
+            basket[index].quantity = value;
+            setBasket(basket);
+            setDirty(true);
+            //setBasket(basket[index].quantity == value);
+        }
+    }
+
+    const handleChange = (event, availability) => {
+        if (event.target.value > 0 && event.target.value < availability) {
+            //setQuantity(event.target.value);
+            let value = event.target.value;
+            setQuantity(value);
+            let isProduct = (product) => product == p;
+            let index = basket.findIndex(isProduct);
+            basket[index].quantity = value;
+            setBasket(basket);
+        }
+    }
+
+    console.log(basket);
+
+    return (
+        <ListGroup.Item as="li" key={p.id}>
+            <Row>
+                <Col xs={2} className="text-left">
+                    <img src={p.img_path} className="img-fluid" style={{ height: "50px", width: "50px" }} />
+                </Col>
+                <Col xs={10} md={4} lg={4} className="text-left">
+                    <strong>{p.name}</strong><br />
+                    <small>ID: {p.id}</small><br />
+                    Price/unity:<strong> € {p.price}</strong><br />
+                </Col>
+                <Col xs={8} md={3} lg={3} className="text-left">
+                    {
+                        !edit ?
+                            /* <div>
+                                Quantity: <strong className="text-right">{p.quantity}</strong><br />
+                                <small>Availability: {p.availability}</small>
+                            </div> */
+                            <div className="text-center">
+                                Quantity:<br />
+                                <ButtonGroup size="sm" aria-label="Basic example" >
+
+                                    <input type="number" id="number" min={p.quantity} max={p.availability} value={quantity} style={{ width: " 40px", height: "40px", textAlign: "center" }} disabled />
+
+                                </ButtonGroup><br />
+                            </div>
+                            :
+                            <div className="text-center">
+                                Quantity:<br />
+                                <ButtonGroup size="sm" aria-label="Basic example" > {/* TODO: FIX INPUT */}
+                                    <Button variant="secondary" style={{ width: " 40px", height: "40px" }} onClick={()=>{decreaseValue(p)}}>-</Button> 
+                                    <input type="number" id="number" min={p.quantity} max={p.availability} value={quantity} onChange={(e)=>{handleChange(e, p.availability)}} style={{ width: " 40px", height: "40px", textAlign: "center" }} />
+                                    <Button variant="info" style={{ width: " 40px", height: "40px" }} onClick={()=>{increaseValue(p)}}>+</Button>
+                                </ButtonGroup><br />
+                                <small>Availability: {p.availability}</small>
+                            </div>
+                    }
+
+                </Col>
+                <Col xs={4} md={3} lg={3} className="text-right">
+                    <ButtonGroup vertical aria-label="Basic example">
+                        {edit? <Button variant="light" onClick={() => { setBasket(basket.filter(product => product !== p)) }}>{iconCross}</Button> : ''}
+                    </ButtonGroup>
+                </Col>
+            </Row>
+        </ListGroup.Item>
     );
 }
 
