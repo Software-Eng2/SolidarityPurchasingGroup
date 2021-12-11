01a829fc5e089d9d03fd2fe87cfea08bfb449b88
sofia-catalano <sofia.catalano@studenti.polito.it>
2021-12-11 09:52:17 +0100

    update virtual clock

--- client/src/components/VirtualClock.js
+++ client/src/components/VirtualClock.js
@@ -8,7 +8,11 @@ import {Clock} from "../Clock.js";
 function VirtualClock(){
     const [hour, setHour] = useState(new Date());
     const [date, setDate] = useState(new Date());
-    const [flagSunday, setFlagSunday] = useState(0);
+
+    const [flagSaturday, setFlagSaturday] = useState(false);
+    const [flagSunday, setFlagSunday] = useState(false);
+    const [flagMonday9, setFlagMonday9] = useState(false);
+    const [flagMonday20, setFlagMonday20] = useState(false);
     const clock = new Clock();
 
 
@@ -21,31 +25,77 @@ function VirtualClock(){
             clearInterval(interval);
         }
     }, []);
+    
 
     function nextWeekdayDate(date, day_in_week) {
-        var ret = new Date(date||new Date());
+        var ret = new Date(date || new Date());
         ret.setDate(ret.getDate() + (day_in_week - 1 - ret.getDay() + 7) % 7 + 1);
         console.log(ret);
         clock.reset(ret);
+        resetFlag();
         return ret;
     }
 
+    function resetFlag(){
+        setFlagSaturday(false);
+        setFlagSunday(false);
+        setFlagMonday9(false);
+        setFlagMonday20(false);
+    }
+
+    function handleSaturday(){
+        clock.setFarmerEstimatesMilestone();
+        setFlagSaturday(true);
+    }
+
+    function handleSunday(){
+        if(flagSaturday === false){
+            clock.setFarmerEstimatesMilestone();
+        }
+        clock.setOrdersAcceptedMilestone();
+        setFlagSunday(true);
+    }
+
+    function handleMonday9(){
+        if(!flagSaturday){
+            clock.setFarmerEstimatesMilestone();
+        }
+        if(!flagSunday){
+            clock.setOrdersAcceptedMilestone();
+        }
+        clock.setAvailabilityConfirmedMilestone();
+        setFlagMonday9(true);
+    }
+
+    function handleMonday20(){
+        if(!flagSaturday){
+            clock.setFarmerEstimatesMilestone();
+        }
+        if(!flagSunday){
+            clock.setOrdersAcceptedMilestone();
+        }
+        if(!flagMonday9){
+            clock.setAvailabilityConfirmedMilestone();
+        }
+        clock.setWalletOKMilestone();
+        setFlagMonday20(true);
+    }
 
 
     return (
         <>
             <ButtonToolbar>
                 <ButtonGroup size="lg" className="clock mx-auto">
-                    <Button variant="outline-success" disabled={flagSunday} onClick={()=> clock.setFarmerEstimatesMilestone()}>
+                    <Button variant="outline-success" disabled={flagSaturday || flagSunday || flagMonday9 || flagMonday20} onClick={() => handleSaturday()}>
                         Saturday 09:00
                     </Button>
-                    <Button variant="outline-success" onClick={()=> {clock.setOrdersAcceptedMilestone(); setFlagSunday(1);}}>
+                    <Button variant="outline-success" disabled={flagSunday || flagMonday9 || flagMonday20} onClick={() => handleSunday()}>
                         Sunday 23:00
                     </Button>
-                    <Button variant="outline-success" disabled={true} onClick={()=> clock.setAvailabilityConfirmedMilestone()}>
+                    <Button variant="outline-success"  disabled={flagMonday9 || flagMonday20} onClick={() => handleMonday9()}>
                         Monday 09:00
                     </Button>
-                    <Button variant="outline-success" disabled={true} onClick={() => clock.setWalletOKMilestone()}>
+                    <Button variant="outline-success" disabled={flagMonday20} onClick={() => handleMonday20()}>
                         Monday 20:00
                     </Button>
                 </ButtonGroup>
@@ -54,7 +104,7 @@ function VirtualClock(){
                 <Calendar onChange={setDate} value={date} minDate={new Date()} minDetail='month' maxDate={new Date(2021, 12, 1)}/>
             </div>
             <div className='d-flex justify-content-center mt-3'>
-                <Button variant="success" className='ml-3 mr-1' onClick={()=> setDate(nextWeekdayDate(date, 2) )} disabled={true}>
+                <Button variant="success" className='ml-3 mr-1' onClick={()=> setDate(nextWeekdayDate(date, 2) )}>
                     Next week
                     <BsFillArrowRightSquareFill className='ml-3 mr-1' size={30} fill="green"/>
                 </Button>
